d4b818d39bdfc2e0ac483f730f2c3b85b9d5f2d8
TodorYanakiev <todoryanakiev395@gmail.com>
2023-06-19 20:50:49 +0300

    Gets the location of the user.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Въведете локацията си в метри: ");
+        int location = 0;
+        Scanner sc = new Scanner(System.in);
+        try{
+            String userLocation = sc.nextLine();
+            location = Integer.parseInt(userLocation.substring(0,userLocation.length()-1));
+        }catch(Exception e){
+            System.out.println("Въведете дължината и съкращението слято(пример: 240m)!");
+            System. exit(0);
+        }
     }
 }
